bd2e491cc9ff0b30bb350e05ec797e67e36f9f13
diego <diegoandrestrom04@gmail.com>
2020-05-04 11:28:34 -0500

    vista homeCourses y dashboardCourses responsive

--- resources/views/templateCourse.blade.php
+++ resources/views/templateCourse.blade.php
@@ -15,26 +15,57 @@
 <body>
     <header class="shadow-sm">
         <div class="container">
-            <nav class="navbar" role="navigation">
-                <div class="navbarHeader">
-                    <h2>LOGO</h2>
-                </div>
+            <div class="">
+                <nav class="navbar navbar-expand-lg" role="navigation">
+                    <div class="navbarHeader">
+                        <h2>LOGO</h2>
+                    </div>
+                    <button class="navbar-toggler first-button" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <div class="animated-icon1"><span></span><span></span><span></span></div>
+                      </button>
 
-                <ul class="nav justify-content-end">
-                    <li class="nav-item"><a href="#">Mis cursos</a></li>
-                    <li class="nav-item">
-                        <a href="#">
-                            <i class="fa fa-envelope"></i><span class="sec counter counter-lg">+9</span>
-                        </a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="#">Diego Rambao 
-                            <img class="img-profile" 
-                                src="{{ asset('img/profile-example.jpg') }}" alt="">
-                        </a>
-                    </li>
-                </ul>
-            </nav>
+                      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                          <ul class="navbar-nav ml-auto">
+                              <li class="nav-item"><a href="#">Mis cursos</a></li>
+                              <li class="nav-item">
+                                  <a href="#">
+                                      <i class="fa fa-envelope"></i><span class="sec counter counter-lg">+9</span>
+                                  </a>
+                              </li>
+                              <li class="nav-item">
+                                  <a href="#">Diego Rambao 
+                                      <img class="img-profile" 
+                                          src="{{ asset('img/profile-example.jpg') }}" alt="">
+                                  </a>
+                              </li>
+                          </ul>
+                      </div>
+    
+                </nav>
+            </div>
+
+            {{-- <div class="d-sm-block d-md-none">
+                <div class="row content-menu">
+                    <div class="navbarHeader">
+                        <h2>LOGO</h2>
+                    </div>
+                    <div>
+                        <button class="navbar-toggler first-button" type="button" data-toggle="collapse" data-target="#navbarSupportedContent20"
+                        aria-controls="navbarSupportedContent20" aria-expanded="false" aria-label="Toggle navigation">
+                        <div class="animated-icon1"><span></span><span></span><span></span></div>
+                        </button>
+    
+                        <div class="collapse navbar-collapse" id="navbarSupportedContent20">
+                            <ul>
+                                <li>Inicio</li>
+                                <li>Mis cursos</li>
+                                <li>Notification</li>
+                                <li>Diego Andres</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div> --}}
         </div>
     </header>
 
@@ -49,5 +80,10 @@
             <p>Todos los derechos reservados @</p>
         </div>
     </footer> --}}
+
+
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 </body>
 </html>
