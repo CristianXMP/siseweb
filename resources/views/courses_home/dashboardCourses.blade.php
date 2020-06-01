97a39915bdffcc43703f3b58dd3cac8974d4ac21
diego <diegoandrestrom04@gmail.com>
2020-06-01 14:17:52 -0500

    Se modifico cards de cursos

--- resources/views/courses_home/dashboardCourses.blade.php
+++ resources/views/courses_home/dashboardCourses.blade.php
@@ -12,21 +12,19 @@
 
 
     <div class="col-lg-4 col-md-6 col-sm-12">
-        <div class="card shadow" style="width: 20rem;">
-            <a href="{{ route('cursoProfesor', $item->subject_id) }}">
-                <img src="{{ asset('img/example2.png') }}" class="card-img-top" alt="...">
-            </a>
+        <div class="card shadow panel.card-linked" style="width: 20rem;">
+            <a href="{{ route('cursoProfesor', $item->subject_id) }}" draggable="false" class="card-link">
+                <div class="card-body">
+                    <div class="title-card-header">
+                        <h5 class="card-title">{{ $item->subject->nombre }}</h5>
+                        <h6 class="font-weight-bold">{{ $item->course->course }} - °{{ $item->course->variation }}</h6>
+                    </div>
+                    <h6 class="mt-1">Profesor: {{ $item->teacher->first_name }} {{ $item->teacher->last_name }}</h6>
 
-            <div class="card-body">
-                <div class="title-card-header">
-                    <h5 class="card-title">{{ $item->subject->nombre }}</h5>
-                    <h6 class="font-weight-bold">{{ $item->course->course }} - °{{ $item->course->variation }}</h6>
+                    <h6 class="mt-1">Periodo: {{ $item->period->nombre }}</h6>
                 </div>
-                <h6 class="mt-3">Profesor: {{ $item->teacher->first_name }} {{ $item->teacher->last_name }}</h6>
-
-                <h6 class="mt-3">Periodo: {{ $item->period->nombre }}</h6>
-            </div>
-          </div>
+            </a>
+        </div>
     </div>
 
     @endforeach
@@ -39,18 +37,19 @@
 
 
     <div class="col-lg-4 col-md-6 col-sm-12">
-        <div class="card shadow" style="width: 20rem;">
-            <a href="{{ route('cursoEstudiante', $item->subject->id) }}">
-                <img src="{{ asset('img/example2.png') }}" class="card-img-top" alt="...">
-            </a>
-            <div class="card-body">
-                <div class="title-card-header">
-                    <h5 class="card-title">{{ $item->subject->nombre }}</h5>
-                    <h6 class="font-weight-bold">{{ $item->course->course }} - °{{ $item->course->variation }}</h6>
+        
+            <div class="card shadow panel.card-linked" style="width: 20rem;">
+                <a href="{{ route('cursoEstudiante', $item->subject->id) }}" class="card-link">
+                <div class="card-body">
+                    <div class="title-card-header">
+                        <h5 class="card-title">{{ $item->subject->nombre }}</h5>
+                        <h6 class="font-weight-bold">{{ $item->course->course }} - °{{ $item->course->variation }}</h6>
+                    </div>
+                    <h6 class="mt-3">Profesor: {{ $item->teacher->first_name }} {{ $item->teacher->last_name }}</h6>
                 </div>
-                <h6 class="mt-3">Profesor: {{ $item->teacher->first_name }} {{ $item->teacher->last_name }}</h6>
+            </a>
             </div>
-          </div>
+        
     </div>
 
     @endforeach
